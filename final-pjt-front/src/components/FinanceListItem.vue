3bca367905cbd9247631e38257b8129bf01b8812
wonnyboi <wonnyboi@naver.com>
2023-11-21 15:41:44 +0900

    [수정] merge 혼선

--- final-pjt-front/src/components/FinanceListItem.vue
+++ final-pjt-front/src/components/FinanceListItem.vue
@@ -1,62 +1,3 @@
-<!-- <template>
-  <div>
-    <div class="table-container">
-      <table>
-        <colgroup>
-          <col style="width: 300px;">
-          <col style="width: 600px;">
-          <col style="width: 150px;">
-        </colgroup>
-        <tbody>
-          <tr>
-            
-            <td>{{ product.kor_co_nm }}</td>
-            <td>
-              <RouterLink :to="{ name: 'FinanceItemDetail', params: { id: product.fin_prdt_cd } }">
-                {{ product.fin_prdt_nm }}
-              </RouterLink>
-            </td>
-            <td>{{ product.dcls_strt_day }}</td>
-            <td v-for="option in product.depositoptions_set">{{ option.intr_rate }} / {{ option.save_trm }}</td>
-            <p>은미야 해줘</p>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-</template>
-
-<script setup>
-import { RouterLink } from 'vue-router';
-console.log('여기까진 잘나옴')
-
-defineProps({ product: Object });
-</script>
-
-<style scoped>
-.table-container {
-  width: 100%;
-  margin: 0 auto;
-}
-
-table {
-  width: 100%;
-  border-collapse: collapse;
-  margin-top: auto;
-}
-
-th, td {
-  border: 1px solid #ddd;
-  padding: 8px;
-  text-align: left;
-}
-
-th {
-  background-color: #f2f2f2;
-}
-</style> -->
-
-
 <template>
   <div>
     <div class="table-container">
@@ -75,6 +16,7 @@ th {
               </RouterLink>
             </td>
             <td>{{ product.dcls_strt_day }}</td>
+            <td v-for="option in product.depositoptions_set">{{ option.intr_rate }} / {{ option.save_trm }}</td>
           </tr>
         </tbody>
       </table>
