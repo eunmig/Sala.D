4fa09f0df9456b3999fca5da9c9bb964191698b9
wonnyboi <wonnyboi@naver.com>
2023-11-21 11:33:36 +0900

    [작업중] 비밀번호 수정 기능-> back에서만 작동

--- final-pjt-front/src/views/ProfileView.vue
+++ final-pjt-front/src/views/ProfileView.vue
@@ -1,17 +1,26 @@
 <template>
-<div> test</div>
+<h3>{{ userData.username }}</h3>
 <div>
-{{ userData.id }}
+  <p>이름: {{ userData.last_name }}{{ userData.first_name }}</p>
+  <p>이메일: {{ userData.email }}</p>
+  <p>연봉: {{ userData.salary }}</p>
+  <PasswordChangePopup />
 </div>
 </template>
   
 <script setup>
-
-
+import PasswordChangePopup from '../components/PasswordChangePopup.vue'
 import { useAuthStore } from '../stores/auth'
-
+import { ref } from 'vue'
 const authStore = useAuthStore()
 const userData = authStore.userData
+
+let showPasswordChangePopup = ref(false)
+
+const open_PopUp = function() {
+  showPasswordChangePopup = !showPasswordChangePopup
+  console.log('팝업')
+}
 console.log(userData.id)
   </script>
   
