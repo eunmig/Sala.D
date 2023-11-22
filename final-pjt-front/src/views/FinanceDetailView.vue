8b1d9b6df3921b76fb3d0511fcd13f6e6f7b8e7e
wonnyboi <wonnyboi@naver.com>
2023-11-22 10:17:45 +0900

    금융상품 관심 목록 등록 기능, deposit option 불필요 데이터 제거하며 저장

--- final-pjt-front/src/views/FinanceDetailView.vue
+++ final-pjt-front/src/views/FinanceDetailView.vue
@@ -1,68 +1,3 @@
-<!-- <template>
-  <button @click="fetchData">새로고침</button>
-  <div>
-    <h1>{{ item?.fin_prdt_nm }}</h1>
-    <p>{{ item?.kor_co_nm }}</p>
-    <p v-html="formattedEtcNote"></p>
-    <OptionList :options="itemOption" />
-  </div>
-</template>
-
-<script setup>
-import axios from 'axios';
-import { ref, computed, onMounted } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
-import { useFinanceStore } from '../stores/finance';
-import { useAuthStore } from '../stores/auth';
-import OptionList from '../components/OptionList.vue';
-
-const authStore = useAuthStore();
-const token = authStore.token;
-const financeStore = useFinanceStore();
-const route = useRoute();
-const router = useRouter();
-const itemOption = ref([]);
-const item = ref(null);
-console.log('item', item)
-
-const formattedEtcNote = computed(() => {
-  return item.value?.etc_note ? item.value.etc_note.replace(/\n/g, '<br>') : '';
-});
-
-const fetchData = async () => {
-  try {
-    const optionsResponse = await axios.get(`${financeStore.API_URL}/finance/deposit-product-options/${route.params.id}/`, {
-      headers: {
-        Authorization: `Token ${token}`
-      }
-    })
-    itemOption.value = optionsResponse.data
-    console.log('상품 옵션 res', optionsResponse.data)
-
-    const productResponse = await axios.get(`${financeStore.API_URL}/finance/deposit-product/${route.params.id}/`, {
-      headers: {
-        Authorization: `Token ${token}`
-      }
-    })
-    item.value = productResponse.data;
-    console.log('금융상품 res', productResponse.data)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-onMounted(() => {
-  fetchData()
-  console.log('onMount: FinanceDetailView')
-})
-
-
-</script>
-
-
-<style scoped>
-
-</style> -->
 <template>
   <h1 class="title">{{ item?.fin_prdt_nm }}</h1>
   <br>
@@ -92,7 +27,7 @@ onMounted(() => {
         </tr>
       </tbody>
     </table>
-    <button @click="toggleLike">{{ isLiked ? 'Unlike' : 'Like' }}</button>
+    <button @click="toggleLike">{{ isLiked ? '관심 상품 해제' : '관심 상품 등록' }}</button>
     <br>
   </div>
 </template>
